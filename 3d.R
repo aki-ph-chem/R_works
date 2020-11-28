7736cef82fa522f97d5e50837ddb4dcce1416fe1
akki <saikurisuto@gmail.com>
2020-11-29 02:37:21 +0900

    エチレンまでやったがいまいち

--- 3d.R
+++ 3d.R
@@ -42,7 +42,79 @@ plot3d(f3$X4,f3$X5,f3$X6,col = fcol[f3[,2]],type = "s")　#分子全体
 
 
 
+######エチレンでやってみる######
 
+ety=read.csv("ety.csv")
+
+library(dplyr)
+
+AN=6 #原子数
+
+
+n=which(ety[,]=="Standard orientation:") %>% max()
+
+
+atm2=ety[(n+5):(n+5+AN-1),]
+
+
+
+atm_n2=atm2[] %>% strsplit(" ") %>% type.convert() ##　文字列を分割して数値に変換
+
+
+
+A=matrix(0,AN,6)
+
+
+f=A
+
+for (i in 1:AN) {for (j in 1:6)
+{
+  f[i,j]=atm_n2[[i]][complete.cases(atm_n2[[i]])][j]
+}
+  
+}
+
+
+##----------------out put-----------------------------
+
+
+
+n
+AN
+f
+
+########エチレンの表示##########
+
+fcol=c(8,7,6,5,4,1)
+
+
+f3=data.frame(f)
+
+
+plot3d(f3$X4,f3$X5,f3$X6,col = fcol[f3[,2]],type = "s")　#分子全体
+
+
+##########エチレンを回転してみる#######
+
+f2=f
+
+n1=f2[4,4:6]-f2[1,4:6]
+
+r_ni=sqrt(sum(n1*n1))
+
+n1=n1/r_ni
+
+for (k in c(2,3,5,6)) {
+  
+  
+  f2[k,4:6]=Rot(4*pi/6)%*%f2[k,4:6]
+  
+}
+
+
+f3=data.frame(f2)
+
+plot3d(f3$X4,f3$X5,f3$X6,col = fcol[f3[,2]],type = "s")
 
 
 
