76f2018a9fd66845922b0ae8a2a65cdd1f443788
akki <saikurisuto@gmail.com>
2020-11-30 13:44:21 +0900

    無事復帰

--- metane2.R
+++ metane2.R
@@ -2,6 +2,78 @@
 ######readLines関数で読み込みプログラムを書いてみた##########
 
 
+metane2=readLines("metane2.txt")
+
+
+library(dplyr)
+
+AN=5 #原子数
+
+
+n=which(metane2[]== "                         Standard orientation:                         " ) %>% max()
+
+
+atm2=metane2[(n+5):(n+5+AN-1)]
+
+
+
+atm_n2=atm2[] %>% strsplit(" ") %>% type.convert() ##　文字列を分割して数値に変換
+
+
+
+A=matrix(0,AN,6)
+
+
+f=A
+
+for (i in 1:AN) {for (j in 1:6)
+{
+  f[i,j]=atm_n2[[i]][complete.cases(atm_n2[[i]])][j]
+}
+  
+}
+
+
+#########out put############
+
+
+
+n
+AN
+f
+
+
+
+#####検索プログラムの改良#####
+
+which(metane2[]== "                         Standard orientation:                         " )
+
+
+ob="           Standard orientation:       "
+
+ob2=ob %>%  strsplit("  ")
+
+
+while (ob2[[1]][]==" Standard orientation:") {
+  
+}
+
+
+
+
+which(ob2[[1]][]==" Standard orientation:")
+
+ob2
+
+
+####メタンを表示####
+
+
+
+f3=data.frame(f)
+
+plot3d(f3$X4,f3$X5,f3$X6,col =f3[,1],type = "s")　#分子全体の表示
+
 
 
 
