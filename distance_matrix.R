6fc2dc15e1846c3ec81a248cf195099551b075ee
akki <saikurisuto@gmail.com>
2020-12-17 11:14:06 +0900

    etyleneで

--- distance_matrix.R
+++ distance_matrix.R
@@ -7,6 +7,29 @@ m
 #Distance mtrixの直前の添え字
 
 
-yy=y[(m+1):(m+1+AN)]
+yy=y[(m+2):(m+1+AN)]%>% str_split(" ") 
 
-yy
+
+
+mm=matrix(0,AN,AN+2)
+
+
+for (k in 1:AN) {
+
+  
+
+n=which(yy[[k]][]!="")
+
+nn=length(n)
+
+nnn=yy[[k]][n]
+
+mm[k,1:nn]=nnn
+
+}
+
+
+A=mm[,3:7] %>% type.convert() %>% round()
+
+
+A
