d17eeb5e115a0b029a7762efbbb625fad240211a
akki <saikurisuto@gmail.com>
2020-12-18 17:10:27 +0900

    気合で原子のナンバリングを調べて回転させました。

--- stb_rot.R
+++ stb_rot.R
@@ -39,15 +39,37 @@ AN
 f
 
 
-#####スチルベンの回転######
+#####stilbene graphics ######
 
 fd=data.frame(f)
 
-plot3d(f[,4],f[,5],f[,6],col = f[,1],type = "s")　#分子全体
 
-carbon=fd %>% filter(fd[,2]==6)
 
-plot3d(carbon[,4],carbon[,5],carbon[,6],col=carbon[,1],type = "s") #炭素骨格のみ
+carbon=fd %>% filter(fd[,2]==6)  #carbon only
+Hydrogen=fd %>% filter(fd[,2]==1) # hydrogen only
+
+y=rep(1,26)
+
+
+y[24]=2
+y[25]=2
+
+plot3d(f[,4],f[,5],f[,6],col =y,type = "s")　#分子全体
+
+
+
+
+
+
+x=rep(1,14)
+
+x[1]=2
+x[2]=2
+
+plot3d(carbon[,4],carbon[,5],carbon[,6],col=x,type = "s") #炭素骨格のみ
+
+
+
 
 
 
@@ -57,19 +79,18 @@ plot3d(carbon[,4],carbon[,5],carbon[,6],col=carbon[,1],type = "s") #炭素骨格
 
 
 
-I=diag(1,3)
 
 n1=c(1,2,3)
-
-n2=rbind(c(0,-n1[3],n1[2]),c(0,0,-n1[1]),c(0,0,0))
-
-n22=-t(n2)
-
-
 
 
 Rot=function(x)
 {
+  I=diag(1,3)
+ 
+   n2=rbind(c(0,-n1[3],n1[2]),c(0,0,-n1[1]),c(0,0,0))
+  
+  n22=-t(n2)
+  
   A=I+sin(x)*(n2+n22)+(n2+n22)%*%(n2+n22)*(1-cos(x))
   
   return(A)
@@ -84,7 +105,7 @@ y=c(1,1,0)
 Rot(pi/2)%*%y
 
 
-######ガウシアンのデータでやってみる####
+######エチレンの軸で回転####
 
 
 f2=f
@@ -95,7 +116,9 @@ r_ni=sqrt(sum(n1*n1))
 
 n1=n1/r_ni
 
-for (k in c(2:26)) {
+z=c(1,26,13:23)
+
+for (k in z) {
   
   
   f2[k,4:6]=Rot(pi/2)%*%f2[k,4:6]
@@ -103,3 +126,14 @@ for (k in c(2:26)) {
 }
 
 f2
+
+
+f2c=f2 %>% data.frame() %>% filter(f2[,2]==6)
+
+x=rep(1,14)
+
+
+
+plot3d(f2c[,4],f2c[,5],f2c[,6],col=x,type = "s") #炭素骨格のみ
+
+
